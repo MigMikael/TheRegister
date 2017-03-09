905c19d40218d4e54d7be19fa4e705631155f3e7
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2017-03-09 15:19:15 +0700

    fix bug redirect after check order

--- resources/views/participant/order.blade.php
+++ resources/views/participant/order.blade.php
@@ -115,7 +115,7 @@
         </div>
         <div class="row">
             <div class="col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1">
-                <a href="{{ url('gain/scan') }}" id="btn-done" class="btn btn-success btn-lg">เสร็จสิ้น</a>
+                <a href="{{ url()->previous() }}" id="btn-done" class="btn btn-success btn-lg">เสร็จสิ้น</a>
             </div>
         </div>
     </div>
